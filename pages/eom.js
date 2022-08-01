8021fe58d3a39b5864649fc1b9a362a95a76d652
Bernardo Acevedo <31902424+ber9795@users.noreply.github.com>
2022-08-01 02:11:25 -0300

    first commit

---
+++ pages/eom.js
@@ -0,0 +1,25 @@
+export const EOM=({ employee})=>{
+    console.log(employee)
+    return (
+        <div className='page-container'>
+        <div><h1>Employee of the month!</h1></div>
+        </div>
+    )
+
+}
+
+export const getServerSideProps = async pageContext => {
+    const apiResponse= await fetch (
+        'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth'
+    
+    )
+    const employee=await apiResponse.json()
+
+    return {
+        props:{
+            employee
+        }
+    }
+    }
+
+export default EOM
